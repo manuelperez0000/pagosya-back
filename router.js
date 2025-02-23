f8ce9beca8c2f6c3242115b3ef87c991cda47ba6
manuel perez <manuelperez.0000@gmail.com>
2025-02-23 13:47:21 -0600

    enviar o transactions

--- router.js
+++ router.js
@@ -4,8 +4,8 @@ import login from './api/user/login.js'
 import getUser from './api/user/getUser.js'
 import register from './api/user/register.js'
 /* import enviar from './api/enviar/enviar.js'
-import transactions from './api/transactions/transactions.js'
-import paymentMethods from './api/methods/methods.js' */
+import transactions from './api/transactions/transactions.js'*/
+import paymentMethods from './api/methods/methods.js' 
 
 const route = express.Router()
 
