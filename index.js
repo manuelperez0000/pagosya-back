911698828f9d32030f7d0845bb6d82a4d70aa1b8
manuel perez <manuelperez.0000@gmail.com>
2025-02-23 13:29:34 -0600

    prueba 666

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dbConnect from './db/conection.js' */
 import 'dotenv/config'; */
 /* import cors from 'cors' */
 
-/* const PORT = process.env.PORT || 3000 */
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
@@ -19,5 +19,6 @@ app.get('/', (req, res) => { res.send('Welcom to PagosYa') })
 /* router(app) */
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+  /* console.log(`Servidor corriendo en http://localhost:${PORT}`) */
+  console.log("servidor corriendo")
 });
