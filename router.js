4aa2b5b41c061ab7d915c757ec8a82a7b12674e7
manuel perez <manuelperez.0000@gmail.com>
2025-02-23 19:39:42 -0600

    hola 3

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ import login from './api/user/login.js'
 import register from './api/user/register.js' */
 /* import paymentMethods from './api/methods/methods.js'  */
 /* import enviar from './api/enviar/enviar.js' */
-/* import transactions from './api/transactions/transactions.js' */
+import transactions from './api/transactions/transactions.js'
 
 const route = express.Router()
 
