c02351e7cc669da01a8e69e32ec7317ebab10cd7
manuel perez <manuelperez.0000@gmail.com>
2025-02-23 20:12:16 -0600

    enviar en conflicto con vercel 11

--- api/transactions/transactionController.js
+++ api/transactions/transactionController.js
@@ -5,11 +5,9 @@ export const saveTransaction = async ({ fromId, toId, fromEmail, toEmail, amount
     const data = { fromId, toId, fromEmail, toEmail, amount }
 
     return await TransactionModel(data).save()
-
-    return true
 }
 
 export const getTransactionsByeEmail = async ({ email }) => {
-    return true
+
     return await TransactionModel.find({ $or: [{ fromEmail: email }, { toEmail: email }] }).sort({ createdAt: -1 })
 }
