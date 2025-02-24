9d1866f1cde480662d95d3722374e50c82747c4e
manuel perez <manuelperez.0000@gmail.com>
2025-02-23 20:09:42 -0600

    enviar en conflicto con vercel 10

--- api/transactions/transactionController.js
+++ api/transactions/transactionController.js
@@ -1,15 +1,15 @@
-/* import TransactionModel from "./transactionModel.js" */
+import TransactionModel from "./transactionModel.js"
 
 export const saveTransaction = async ({ fromId, toId, fromEmail, toEmail, amount }) => {
 
-    /* const data = { fromId, toId, fromEmail, toEmail, amount }
+    const data = { fromId, toId, fromEmail, toEmail, amount }
 
-    return await TransactionModel(data).save() */
+    return await TransactionModel(data).save()
 
     return true
 }
 
 export const getTransactionsByeEmail = async ({ email }) => {
     return true
-    /* return await TransactionModel.find({ $or: [{ fromEmail: email }, { toEmail: email }] }).sort({ createdAt: -1 }) */
+    return await TransactionModel.find({ $or: [{ fromEmail: email }, { toEmail: email }] }).sort({ createdAt: -1 })
 }
