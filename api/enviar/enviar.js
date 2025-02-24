631f26aa1763c9dee7b10af367e6c033b55bd16f
manuel perez <manuelperez.0000@gmail.com>
2025-02-23 19:50:37 -0600

    enviar en conflicto con vercel

--- api/enviar/enviar.js
+++ api/enviar/enviar.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import responser from '../../network/response.js'
-import { enviar, confirm, saveContact } from './controller.js'
+/* import { enviar, confirm, saveContact } from './controller.js' */
 import validateToken from '../../midelwares/validateToken.js'
-import { saveTransaction } from '../transactions/transactionController.js'
-import { getUserIdFromEmail } from '../user/store/controller.js'
+/* import { saveTransaction } from '../transactions/transactionController.js'
+import { getUserIdFromEmail } from '../user/store/controller.js' */
 
 const router = express.Router()
 
 router.post('/', validateToken, async (req, res) => {
     try {
-        const { to, amount, checked } = req.body
+        /* const { to, amount, checked } = req.body
 
         const from = req.user.email
 
@@ -27,9 +27,9 @@ router.post('/', validateToken, async (req, res) => {
             responser.success({ res, message: "Enviado con exito", body: { from, to, amount, info: response, status: true } })
         } else {
             throw 'Error al realizar el envio'
-        }
+        } */
 
-
+        responser.success({ res, message: "Enviado con exito", body: {} })
     } catch (error) {
         responser.error({ res, message: error?.message || error })
     }
@@ -38,20 +38,24 @@ router.post('/', validateToken, async (req, res) => {
 router.post('/confirm', validateToken, async (req, res) => {
 
     try {
-        const { to, amount } = req.body
+        /* 
+    
+            const { to, amount } = req.body
+    
+            const from = req.user.email
+    
+            const response = await confirm({ from, to, amount })
+    
+            const { message } = response
+    
+            if (response.confirm) {
+                responser.success({ res, message, body: response })
+            } else {
+                responser.error({ res, message, status: 400, body: response })
+            }
+     */
 
-        const from = req.user.email
-
-        const response = await confirm({ from, to, amount })
-
-        const { message } = response
-
-        if (response.confirm) {
-            responser.success({ res, message, body: response })
-        } else {
-            responser.error({ res, message, status: 400, body: response })
-        }
-
+        responser.success({ res, message: "", body: {} })
     } catch (error) {
         responser.error({ res, message: error?.message || error })
     }
--- router.js
+++ router.js
@@ -4,11 +4,9 @@ import login from './api/user/login.js'
 import getUser from './api/user/getUser.js'
 import register from './api/user/register.js'
 import paymentMethods from './api/methods/methods.js' 
-
-
-
 import transactions from './api/transactions/transactions.js'
-/* import enviar from './api/enviar/enviar.js' */
+
+import enviar from './api/enviar/enviar.js' //el problema
 
 const route = express.Router()
 
@@ -17,17 +15,17 @@ const router = (app) => {
     app.use('/api/v1', route)
     route.use('/user/login', login)
 
-    /* app.use(cors()) */
-    /* 
+    app.use(cors())
+    
     route.use('/user/register', register)
     route.use('/user/getUser', getUser)
     route.use('/enviar', enviar)
     route.use('/transactions', transactions)
-    route.use('/paymentMethods', paymentMethods) */
+    route.use('/paymentMethods', paymentMethods)
 
-    /* route.use('*', (req, res)=>{ 
+    route.use('*', (req, res)=>{ 
         responser.error({ res, message: error?.message || error })
-     }) */
+     })
 }
 
 export default router
