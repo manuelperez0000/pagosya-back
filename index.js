50fd14a4b4633ab06c537a90297cfef070c0295d
manuel perez <manuelperez.0000@gmail.com>
2025-10-31 08:09:31 -0400

    servidor con node

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ import router from './router.js'
 import morgan from 'morgan';
 import { Server } from 'socket.io';
 import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const PORT = process.env.PORT || 3000
@@ -31,11 +36,17 @@ app.use(morgan('dev'))
 
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, 'dist')))
+
 dbConnect()
 
 app.get('/', (req, res) => { res.send('Welcom to PagosYa') })
 
 router(app)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
 
 server.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
